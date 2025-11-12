d429c3873bb558690923b94317242992bc38a8f8
Thyago <thyagorps@gmail.com>
2025-11-12 11:45:01 -0300

    refactor: animação do texto do banner

--- src/components/Banner/index.jsx
+++ src/components/Banner/index.jsx
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import logo from "/assets/logo-branco.svg";
+import { useEffect, useState } from "react";
 
 const BannerStyled = styled.header`
   width: 100vw;
@@ -8,6 +9,17 @@ const BannerStyled = styled.header`
   justify-content: center;
   align-items: center;
   padding: 2rem 0;
+  position: relative;
+  &::before {
+    content: "";
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: #6278f77f;
+    z-index: 0;
+  }
 `;
 
 const BackgroundStyled = styled.div`
@@ -29,8 +41,9 @@ const ContentStyled = styled.div`
   justify-content: center;
   align-items: start;
   width: 66%;
-  gap: 3rem;
+  gap: 2rem;
   color: var(--cor-grafite);
+  z-index: 1;
   img {
     height: 6rem;
   }
@@ -40,13 +53,57 @@ const ContentStyled = styled.div`
 `;
 
 const Banner = () => {
+  const [palavraDinamica, setPalavraDinamica] = useState('')
+  
+  const pausa = (ms) => new Promise(resolve => setTimeout(resolve, ms));  
+  
+  useEffect(() => {
+    const palavras = ['times', 'grupos', 'equipes']
+    
+    let ativo = true; // controle pra evitar erros ao desmontar
+
+    const escreverPalavra = async (palavra) => {
+      let palavraAtual = ''
+  
+      for (let letra of palavra) {
+        palavraAtual += letra
+        setPalavraDinamica(palavraAtual)
+        await pausa(200) // tempo entre letras
+      }
+  
+      await pausa(1000) // tempo antes de apagar
+      await apagarPalavra(palavra);
+    }
+
+    const apagarPalavra = async (palavra) => {
+      for (let i = palavra.length; i >= 0; i--) {
+        setPalavraDinamica(palavra.slice(0, i))
+        await pausa(100); // tempo entre apagar cada letra
+      }
+    };
+
+    const escreverLista = async () => {
+      while (ativo) {
+        for (let palavra of palavras) {
+          await escreverPalavra(palavra)
+        }
+      }
+    }
+
+    escreverLista()
+
+    return () => {
+      ativo = false; // evita loop após desmontagem
+    }
+  }, [])
+
   return (
     <BannerStyled>
       <BackgroundStyled>
         <ContentStyled>
           <img src={logo} alt="Logo Organo" />
           <div>
-            <h2>Pessoas e times</h2>
+            <h2>Pessoas e {palavraDinamica}</h2>
             <h3>organizados em um só lugar!</h3>
           </div>
         </ContentStyled>
--- src/context/GlobalStyles.jsx
+++ src/context/GlobalStyles.jsx
@@ -46,12 +46,12 @@ h1, h2, h3 {
 
 h2 {
   font-size: 4rem;
-  line-height: 4.5rem;
+  line-height: 5rem;
 }
 
 h3 {
   font-size: 2rem;
-  line-height: 2.5rem;
+  line-height: 3rem;
 }
 
 .section {
